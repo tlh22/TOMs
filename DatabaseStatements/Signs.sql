570f7f0355ebeee57be02cc454d57a05b6c5a873
tlh22 <thancock@mhtc.co.uk>
2017-08-03 18:50:27 +0200

    Add database create statements

---
+++ DatabaseStatements/Signs.sql
@@ -0,0 +1,20 @@
+﻿-- Table: public."Signs"
+
+-- DROP TABLE public."Signs";
+
+CREATE TABLE public."Signs"
+(
+  id integer NOT NULL DEFAULT nextval('"Signs_id_seq"'::regclass),
+  geometry geometry(Point,27700),
+  "GeometryID" character varying,
+  "Type_1" character varying,
+  "Type_2" character varying,
+  "Type_3" character varying,
+  "Photos" character varying,
+  CONSTRAINT "Signs_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."Signs"
+  OWNER TO postgres;
---
+++ DatabaseStatements/TOMs_Layer.sql
@@ -0,0 +1,35 @@
+﻿-- Table: public."TOMs_Layer"
+
+-- DROP TABLE public."TOMs_Layer";
+
+CREATE TABLE public."TOMs_Layer"
+(
+  id integer NOT NULL DEFAULT nextval('"TOMs_Layer2_id_seq"'::regclass),
+  geom geometry(MultiLineString,27700),
+  "GeometryID" character varying(254),
+  "RestrictionTypeID" bigint,
+  "CPZ" character varying(254),
+  "TimePeriodID" bigint,
+  "MaxStayID" bigint,
+  "PayTypeID" bigint,
+  "GeomTypeID" bigint,
+  "RoadName" character varying(254),
+  "Length" numeric,
+  "EffectiveDate" character varying(254),
+  "RescindDate" character varying(254),
+  "RestrictionStatusID" bigint,
+  "LengthFeat" numeric,
+  shape_leng numeric,
+  geometry_l numeric,
+  "ChangeNotes" character varying(254),
+  "NoReturnID" bigint,
+  "Orientation" character varying(254),
+  "AzimuthToRoadCentreLine" character varying(254),
+  "ChangeDate" character varying(254),
+  CONSTRAINT "TOMs_Layer_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."TOMs_Layer"
+  OWNER TO postgres;
