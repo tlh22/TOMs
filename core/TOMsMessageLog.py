16ded9ce711e1ee5ce287b4ed97b3f3a1d28cbb5
tlh22 <thancock@mhtc.co.uk>
2020-06-16 15:49:38 +0200

    Update messageLog

--- core/TOMsMessageLog.py
+++ core/TOMsMessageLog.py
@@ -22,6 +22,7 @@
 import time, datetime
 import functools
 import os.path
+import sys
 
 from qgis.core import (
     Qgis,
@@ -34,24 +35,29 @@ from qgis.core import (
 class TOMsMessageLog(QgsMessageLog):
 
     def __init__(self):
-        super().__init__(*args, **kwargs)
+        super().__init__()
 
+    @staticmethod
     def logMessage(*args, **kwargs):
         # check to see if a logging level has been set
         def currentLoggingLevel():
-            """
-            Returns the current proposal
-            """
-            currLoggingLevel = QgsExpressionContextUtils.projectScope(QgsProject.instance()).variable('TOMsDebugLevel')
+            try:
+                currLoggingLevel = QgsExpressionContextUtils.projectScope(QgsProject.instance()).variable('TOMs_Logging_Level')
+            except Exception as e:
+                QgsMessageLog.logMessage("Error in TOMsMessageLog. TOMs_logging_Level not found ... ", tag="TOMs Panel")
+
             if not currLoggingLevel:
                 currLoggingLevel = Qgis.Warning
             return int(currLoggingLevel)
 
         debug_level = currentLoggingLevel()
 
-        messageLevel = kwargs.get('level')
+        try:
+            messageLevel = kwargs.get('level')
+        except Exception as e:
+            QgsMessageLog.logMessage("Error in TOMsMessageLog level not found...", tag="TOMs Panel")
 
-        # QgsMessageLog.logMessage('{}: messageLevel: {}; debug_level: {}'.format(args[0], messageLevel, debug_level), tag="TOMs panel")
+        #QgsMessageLog.logMessage('{}: messageLevel: {}; debug_level: {}'.format(args[0], messageLevel, debug_level), tag="TOMs panel")
 
         if not messageLevel:
             messageLevel = Qgis.Info
@@ -59,20 +65,23 @@ class TOMsMessageLog(QgsMessageLog):
         if messageLevel >= debug_level:
             QgsMessageLog.logMessage(*args, **kwargs, tag="TOMs Panel")
 
+    def setLogFile(self):
+
+        try:
             logFilePath = os.environ.get('QGIS_LOGFILE_PATH')
+        except Exception as e:
+            QgsMessageLog.logMessage("Error in TOMsMessageLog. QGIS_LOGFILE_PATH not found ... ", tag="TOMs Panel")
 
-            if logFilePath:
+        if logFilePath:
+            QgsMessageLog.logMessage("LogFilePath: " + str(logFilePath), tag="TOMs Panel", level=Qgis.Info)
 
-                #QgsMessageLog.logMessage("LogFilePath: " + str(logFilePath), tag="TOMs panel")
+            logfile = 'qgis_' + datetime.date.today().strftime("%Y%m%d") + '.log'
+            self.filename = os.path.join(logFilePath, logfile)
+            QgsMessageLog.logMessage("Sorting out log file" + self.filename, tag="TOMs Panel", level=Qgis.Info)
+            QgsApplication.messageLog().messageReceived.connect(self.write_log_message)
 
-                logfile = 'qgis_' + datetime.date.today().strftime("%Y%m%d") + '.log'
-                filename = os.path.join(logFilePath, logfile)
-                #QgsMessageLog.logMessage("Sorting out log file" + filename, tag="TOMs panel")
-                #QgsApplication.instance().messageLog().messageReceived.connect(self.write_log_message)  # For some reason, when this is used outside, it crashes QGIS - without any messages ...
-                with open(filename, 'a') as logfile:
-                    logfile.write(
-                        '{dateDetails}[{tag}]: {level} :: {message}\n'.format(
-                            dateDetails=time.strftime("%Y%m%d:%H%M%S"),
-                            tag=kwargs.get('tag'), level=kwargs.get('level'), message=''.join(str(e) for e in args)))
-
-
+    def write_log_message(self, message, tag, level):
+        with open(self.filename, 'a') as logfile:
+            logfile.write(
+                '{dateDetails}[{tag}]: {level} :: {message}\n'.format(dateDetails=time.strftime("%Y%m%d:%H%M%S"),
+                                                                      tag=tag, level=level, message=message))
