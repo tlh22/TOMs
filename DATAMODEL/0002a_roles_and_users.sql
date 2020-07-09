35b31a142057dd49c26b0a9054813e1ef1767281
tlh22 <thancock@mhtc.co.uk>
2020-07-09 09:57:11 +0200

    Now running
    Happy that this now starts correctly and the database is populated. Still issue with accessing db

--- DATAMODEL/0002a_roles_and_users.sql
+++ DATAMODEL/0002a_roles_and_users.sql
@@ -1,3 +1,5 @@
 CREATE ROLE toms_admin NOLOGIN;
 CREATE ROLE toms_operator NOLOGIN;
 CREATE ROLE toms_public NOLOGIN;
+CREATE USER "toms.test" WITH PASSWORD 'password';
+GRANT toms_public TO "toms.test";
---
+++ DOCKER/toms_test_db_init_scripts/0000_create_and_populate_toms_test_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+psql -U postgres -c 'create database "TOMs_Test";'
+
+psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0001_initial_data_structure.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002a_roles_and_users.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002b_permissions.sql"
+
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002a_test_data_lookups.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b1_test_data_main.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b2_test_data_main.sql"
+
+exit
