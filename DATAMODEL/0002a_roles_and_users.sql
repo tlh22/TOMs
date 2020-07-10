ac515920eecebb8040d1eb18e3498d98c89e4953
tlh22 <thancock@mhtc.co.uk>
2020-07-10 07:33:00 +0200

    Further adjustments

--- DATAMODEL/0002a_roles_and_users.sql
+++ DATAMODEL/0002a_roles_and_users.sql
@@ -1,5 +1,9 @@
 CREATE ROLE toms_admin NOLOGIN;
 CREATE ROLE toms_operator NOLOGIN;
 CREATE ROLE toms_public NOLOGIN;
-CREATE USER "toms.test" WITH PASSWORD 'password';
-GRANT toms_public TO "toms.test";
+CREATE USER "toms.public" WITH PASSWORD 'password';
+GRANT toms_public TO "toms.public";
+CREATE USER "toms.operator" WITH PASSWORD 'password';
+GRANT toms_operator TO "toms.operator";
+CREATE USER "toms.admin" WITH PASSWORD 'password';
+GRANT toms_admin TO "toms.admin";
