d7c3c684f921b23de83b77d656d9333e77f95eea
tlh22 <thancock@mhtc.co.uk>
2020-07-14 16:54:26 +0200

    sync ...

--- QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0001_create_toms_test_db.sh
+++ QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0001_create_toms_test_db.sh
@@ -5,7 +5,3 @@ psql -U postgres -c 'create database "TOMs_Test";'
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0001_initial_data_structure.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002a_roles_and_users.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002b_permissions.sql"
-
-psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002a_test_data_lookups.sql"
-psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b1_test_data_main.sql"
-psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b2_test_data_main.sql"
---
+++ QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0002_populate_toms_test_db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+psql -U postgres -c 'create database "TOMs_Test";'
+
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002a_test_data_lookups.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b1_test_data_main.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b2_test_data_main.sql"
