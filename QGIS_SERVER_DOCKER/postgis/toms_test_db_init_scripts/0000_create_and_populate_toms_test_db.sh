d456d1722c579af5c2ba443345b2560377fed20c
tlh22 <thancock@mhtc.co.uk>
2020-07-14 16:41:18 +0200

    Working ...

--- QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0000_create_and_populate_toms_test_db.sh
+++ QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0000_create_and_populate_toms_test_db.sh
@@ -9,5 +9,3 @@ psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002b_permissions.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002a_test_data_lookups.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b1_test_data_main.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b2_test_data_main.sql"
-
-exit
