e72fd89fa2782b1b211de1c42a8e2d9131cfd976
tlh22 <thancock@mhtc.co.uk>
2020-07-16 21:55:55 +0200

    Windows-Linux issue

--- QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0001_create_toms_test_db.sh
+++ QGIS_SERVER_DOCKER/postgis/toms_test_db_init_scripts/0001_create_toms_test_db.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 psql -U postgres -c 'create database "TOMs_Test";'
-
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0001_initial_data_structure.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002a_roles_and_users.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002b_permissions.sql"
