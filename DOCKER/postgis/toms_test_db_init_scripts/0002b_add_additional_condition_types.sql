b1c89e423522f339b651366dd12b17d15d1507fb
tlh22 <thancock@mhtc.co.uk>
2020-09-24 17:52:45 +0200

    Create 0002b_add_additional_condition_types.sql

---
+++ DOCKER/postgis/toms_test_db_init_scripts/0002b_add_additional_condition_types.sql
@@ -0,0 +1,5 @@
+-- add some test values for "AdditionalConditionTypes"
+
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (1, 'except buses');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (2, 'on school entrance markings');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (3, 'on markings');
