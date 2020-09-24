1733848595eec3b82d3cad4e6ccbf8e59f9e497f
tlh22 <thancock@mhtc.co.uk>
2020-09-24 22:02:55 +0200

    add new .sh file

--- DOCKER/postgis/toms_test_db_init_scripts/0001_create_toms_test_db.sh
+++ DOCKER/postgis/toms_test_db_init_scripts/0001_create_toms_test_db.sh
@@ -7,4 +7,4 @@ psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002c_additional_permission
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0002d_tidy_itn_roadcentreline.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0006a_restructure_compliance_lookups.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0006c_additional_toms_details.sql"
-psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0015_add_toms_create_date.sql"
+
