a7d3e80957d2b84c2ac78ccd24b55066d3cb21c2
tlh22 <thancock@mhtc.co.uk>
2020-10-07 18:40:39 +0200

    ... and again

--- DOCKER/postgis/toms_test_db_init_scripts/0002a_populate_toms_test_db.sh
+++ DOCKER/postgis/toms_test_db_init_scripts/0002a_populate_toms_test_db.sh
@@ -2,4 +2,3 @@
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002a_test_data_lookups.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b1_test_data_main.sql"
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0002b2_test_data_main.sql"
-
