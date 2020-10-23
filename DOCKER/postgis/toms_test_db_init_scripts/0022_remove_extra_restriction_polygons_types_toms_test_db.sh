7abe7c3a6ae6ffa2646d19e3d081d3b292be1dcd
tlh22 <thancock@mhtc.co.uk>
2020-10-23 15:21:23 +0200

    More tidying ...

--- DOCKER/postgis/toms_test_db_init_scripts/0022_remove_extra_restriction_polygons_types_toms_test_db.sh
+++ DOCKER/postgis/toms_test_db_init_scripts/0022_remove_extra_restriction_polygons_types_toms_test_db.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0022_remove_extra_restriction_polygon_types.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0023_resolve_cpz_print_issue.sql"
