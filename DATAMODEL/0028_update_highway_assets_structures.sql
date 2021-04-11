c59ed1b818ad9e181ad2e98d9595c327c18882f5
tlh22 <thancock@mhtc.co.uk>
2021-04-11 19:21:13 +0200

    Update 0028_update_highway_assets_structures.sql

--- DATAMODEL/0028_update_highway_assets_structures.sql
+++ DATAMODEL/0028_update_highway_assets_structures.sql
@@ -25,7 +25,7 @@ WHERE geom_linestring IS NOT NULL;
 
 ALTER TABLE highway_assets."SubterraneanFeatures" ENABLE TRIGGER all;
 
--- add polygon type of TrafficCalming
+-- add polygon type to TrafficCalming
 
 ALTER TABLE highway_assets."TrafficCalming"
     ADD COLUMN "geom_polygon" "public"."geometry"(Polygon,27700);
@@ -34,3 +34,17 @@ ALTER TABLE highway_assets."TrafficCalming"
 
 ALTER TABLE highway_assets."Bridges"
     ADD COLUMN "geom_polygon" "public"."geometry"(Polygon,27700);
+
+-- add extra columns to BusShelters
+
+ALTER TABLE highway_assets."BusShelters"
+    ADD COLUMN "geom_polygon" "public"."geometry"(Polygon,27700);
+ALTER TABLE highway_assets."BusShelters"
+    ADD COLUMN "GeomShapeID" integer;
+ALTER TABLE highway_assets."BusShelters"
+    ADD COLUMN "AzimuthToRoadCentreLine" double precision;
+ALTER TABLE highway_assets."BusShelters"
+    ADD COLUMN "BayWidth" double precision;
+
+ALTER TABLE ONLY highway_assets."BusShelters"
+    ADD CONSTRAINT "BusShelters_GeomShapeID_fkey" FOREIGN KEY ("GeomShapeID") REFERENCES "toms_lookups"."RestrictionGeomShapeTypes"("Code");
