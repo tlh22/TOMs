e4d12fe7743da7807f1e93f3cfcf39c7f80e5b78
tlh22 <thancock@mhtc.co.uk>
2021-04-22 09:50:37 +0200

    triggers

--- DATAMODEL/Specials/9998_disable_triggers.sql
+++ DATAMODEL/Specials/9998_disable_triggers.sql
@@ -3,6 +3,15 @@ ALTER TABLE toms."Lines" DISABLE TRIGGER all;
 ALTER TABLE toms."Signs" DISABLE TRIGGER all;
 ALTER TABLE toms."RestrictionPolygons" DISABLE TRIGGER all;
 
+-- moving traffic
+ALTER TABLE moving_traffic."CarriagewayMarkings" DISABLE TRIGGER all;
+ALTER TABLE moving_traffic."AccessRestrictions" DISABLE TRIGGER all;
+ALTER TABLE moving_traffic."RestrictionsForVehicles" DISABLE TRIGGER all;
+ALTER TABLE moving_traffic."TurnRestrictions" DISABLE TRIGGER all;
+ALTER TABLE moving_traffic."HighwayDedications" DISABLE TRIGGER all;
+ALTER TABLE moving_traffic."SpecialDesignations" DISABLE TRIGGER all;
+
+-- Highway Assets
 ALTER TABLE highway_assets."CrossingPoints" DISABLE TRIGGER all;
 ALTER TABLE highway_assets."PedestrianRailings" DISABLE TRIGGER all;
 ALTER TABLE highway_assets."TrafficCalming" DISABLE TRIGGER all;
--- DATAMODEL/Specials/9999_enable_triggers.sql
+++ DATAMODEL/Specials/9999_enable_triggers.sql
@@ -3,6 +3,15 @@ ALTER TABLE toms."Lines" ENABLE TRIGGER all;
 ALTER TABLE toms."Signs" ENABLE TRIGGER all;
 ALTER TABLE toms."RestrictionPolygons" ENABLE TRIGGER all;
 
+-- moving traffic
+ALTER TABLE moving_traffic."CarriagewayMarkings" ENABLE TRIGGER all;
+ALTER TABLE moving_traffic."AccessRestrictions" ENABLE TRIGGER all;
+ALTER TABLE moving_traffic."RestrictionsForVehicles" ENABLE TRIGGER all;
+ALTER TABLE moving_traffic."TurnRestrictions" ENABLE TRIGGER all;
+ALTER TABLE moving_traffic."HighwayDedications" ENABLE TRIGGER all;
+ALTER TABLE moving_traffic."SpecialDesignations" ENABLE TRIGGER all;
+
+-- Highway Assets
 ALTER TABLE highway_assets."CrossingPoints" ENABLE TRIGGER all;
 ALTER TABLE highway_assets."PedestrianRailings" ENABLE TRIGGER all;
 ALTER TABLE highway_assets."TrafficCalming" ENABLE TRIGGER all;
