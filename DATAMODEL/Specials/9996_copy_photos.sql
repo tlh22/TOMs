2d1594d04b0a210af602d6e8369e24b077e172ef
tlh22 <thancock@mhtc.co.uk>
2021-04-22 09:28:05 +0200

    Update 9996_copy_photos.sql

--- DATAMODEL/Specials/9996_copy_photos.sql
+++ DATAMODEL/Specials/9996_copy_photos.sql
@@ -2,7 +2,7 @@
 To copy all required photos, steps are:
  1. Generate a file containining the list of files required
   - Possibly created from query in 9997_check_photos.sql
-  - Ensure that file line endings of system
+  - Ensure that file line endings of system (linux/windows)
 
  2. Copy for required folder/directory
   - for Linux use:
@@ -12,5 +12,7 @@ To copy all required photos, steps are:
       while IFS= read -r line; do cp "$prefix/$line" /home/tim/ISL_Photos_210420; done < $file
 
   - for Windows ...
+
+ 3. For Islington, due to number of files, it was not possible to zip the whole folder/directory. Instead, files were grouped by month ...
 
 */
