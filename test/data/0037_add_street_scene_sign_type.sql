473faaf58bf6a5ecedd2226df63c093f5a224cbb
tlh22 <thancock@mhtc.co.uk>
2021-07-09 08:57:49 +0200

    extra details

--- test/data/0037_add_street_scene_sign_type.sql
+++ test/data/0037_add_street_scene_sign_type.sql
@@ -3,3 +3,8 @@
 INSERT INTO toms_lookups."SignTypes"(
 	"Code", "Description")
 	VALUES (9999, 'Street Scene');
+
+-- Add generic SRL type
+INSERT INTO toms_lookups."BayLineTypes"(
+	"Code", "Description")
+	VALUES (226, 'No stopping (SRL)');
