a1059a67a77f80b5b6db6e380d29569042c2015f
tlh22 <thancock@mhtc.co.uk>
2021-08-21 21:48:46 +0200

    ssl

---
+++ DOCKER/postgis/ssl/prepare_folders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# https://info.crunchydata.com/blog/ssl-certificate-authentication-postgresql-docker-containers
+
+# This directory is optional, but will use it to keep the CA root key safe
+mkdir -p /home/pg_config
+mkdir keys certs
+chmod og-rwx keys certs
+
+# Set up a directory that will serve as the pgconf mount
+mkdir -p /home/pg_config/pgconf
