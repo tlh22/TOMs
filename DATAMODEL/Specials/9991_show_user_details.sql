2a61af1743193c32ee72ae7e082205186e2f3acc
tlh22 <thancock@mhtc.co.uk>
2021-09-09 11:06:45 +0200

    Include trigger for DisplayLabel default

--- DATAMODEL/Specials/9991_show_user_details.sql
+++ DATAMODEL/Specials/9991_show_user_details.sql
@@ -1,7 +1,9 @@
 
+/**
+Show user role
+**/
 -- https://stackoverflow.com/questions/3554484/postgresql-query-to-show-the-groups-of-a-user
 select rolname from pg_user
 join pg_auth_members on (pg_user.usesysid=pg_auth_members.member)
 join pg_roles on (pg_roles.oid=pg_auth_members.roleid)
-where
-pg_user.usename='gavin.reid';
+where pg_user.usename='user.name';
