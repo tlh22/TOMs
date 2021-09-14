2ca0d6e0237364796d55ff5065459478c0a541ff
tlh22 <thancock@mhtc.co.uk>
2021-09-14 09:57:04 +0200

    Extras

---
+++ test/data/0043_add_unacceptable_types.sql
@@ -0,0 +1,5 @@
+/**
+Additional Unacceptable types
+**/
+INSERT INTO "toms_lookups"."UnacceptableTypes" ("Code", "Description") VALUES (11, 'Informal Crossover (vehicles)');
+INSERT INTO "toms_lookups"."UnacceptableTypes" ("Code", "Description") VALUES (12, 'Distributor Road');
