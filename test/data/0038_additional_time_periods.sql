dc0fea3169a3a698efe21aa312208858d191a692
tlh22 <thancock@mhtc.co.uk>
2021-09-28 14:42:39 +0200

    Update 0038_additional_time_periods.sql

--- test/data/0038_additional_time_periods.sql
+++ test/data/0038_additional_time_periods.sql
@@ -251,3 +251,4 @@ INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VA
 INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (613, 'Mon-Sun 9.00am-7.00pm', '9.00am-7.00pm');
 INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (614, 'Mon-Sun 8.00am-6.00pm', '8.00am-6.00pm');
 INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (615, 'Mon-Sun 1.00pm-8.00pm', '1.00pm-8.00pm');
+INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (616, 'Mon-Fri 8.00am-4.15pm', 'Mon-Fri 8.00am-4.15pm');
