63d388237a6f48647a7b70e5866a0daf55033ce6
tlh22 <thancock@mhtc.co.uk>
2021-10-29 09:44:36 +0200

    Update 0015_add_toms_create_date.sh

--- DOCKER/postgis/toms_test_db_init_scripts/0015_add_toms_create_date.sh
+++ DOCKER/postgis/toms_test_db_init_scripts/0015_add_toms_create_date.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0015_add_toms_create_date.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0016_add_moving_traffic_create_date.sql"
+#psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0017_add_highway_assets_create_date.sql"
