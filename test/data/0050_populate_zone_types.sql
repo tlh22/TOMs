3ad68a1df065c20525b64eaa647499852d2891f3
tlh22 <thancock@mhtc.co.uk>
2021-11-22 14:16:44 +0100

    Update 0050_populate_zone_types.sql

--- test/data/0050_populate_zone_types.sql
+++ test/data/0050_populate_zone_types.sql
@@ -2,17 +2,20 @@
 Use same codes as RestrictionPolygonTypes
 **/
 
-INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (20, 'Controlled Parking Zone');
 INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (7, 'Lorry waiting restriction zone');
 INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (8, 'Half-on/Half-off prohbited zone');
+INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (10, 'Restricted Zone');
+
+INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (20, 'Controlled Parking Zone');
 INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (22, 'Parking Tariff Area');
 INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (21, 'Priority Parking Area');
+INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (32, 'Match Day/Event Day Zone');
 
 INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (23, 'Congestion Charging Zone');
 INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (24, 'Ultra Low Emissions Zone');
-INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (10, 'Restricted Zone');
 
-INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (32, 'Match Day/Event Day Zone');
+INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (33, 'Speed Zone');
+INSERT INTO "toms_lookups"."ZoneTypes" ("Code", "Description") VALUES (34, 'Weight Limit Zone');
 
 -- Set next sequence value for ZoneTypesID
 SELECT setval('toms_lookups."ZoneTypes_id_seq"', 33, true);
