8076c497fcc129d974192698a8ff4cf7767a1ef2
tlh22 <thancock@mhtc.co.uk>
2021-12-09 21:26:51 +0100

    Update 0040_more_additional_conditions.sql

--- test/data/0040_more_additional_conditions.sql
+++ test/data/0040_more_additional_conditions.sql
@@ -16,9 +16,9 @@ INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VA
 
 SELECT pg_catalog.setval('"toms_lookups"."AdditionalConditionTypes_Code_seq"', 1, false);
 
-INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (13, 'No Stopping 7.00am-7.00pm;Except 7.00am-4.00pm;Loading max 20 min;Disabled max 3 hours');
-INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (14, 'No Stopping 7.00am-7.00pm;Except 7.00am-4.00pm');
-INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (15, 'No Stopping 7.00am-7.00pm;Except 10.00am-7.00pm');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (13, 'Except 7.00am-4.00pm;Loading max 20 min;Disabled max 3 hours');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (14, 'Except 7.00am-4.00pm');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (15, 'Except 10.00am-7.00pm');
 
 INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (16, 'Resident Doctor and permit holders only');
 INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (17, 'except Ambulances');
@@ -26,3 +26,14 @@ INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VA
 INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (19, 'except for access');
 INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (20, 'except for loading');
 INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (21, 'except ambulances');
+
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (22, 'Loading max 20 min');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (23, 'Disabled max 3 hours');
+
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (24, 'Loading max 20 min;Disabled max 3 hours');
+
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (25, 'Except 10.00am-4.00pm;Loading max 20 min');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (26, 'Except 1.00pm-4.00pm;Loading max 20 min');
+
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (27, 'except disabled');
+INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (27, 'except e-taxis');
