aa79a0ddc4d7430217e792a5c7dfba5eead026fb
tlh22 <thancock@mhtc.co.uk>
2021-12-09 11:23:05 +0100

    Update 0043_additional_unacceptable_types.sql

--- test/data/0043_additional_unacceptable_types.sql
+++ test/data/0043_additional_unacceptable_types.sql
@@ -3,5 +3,5 @@ Include cycle lane
 **/
 
 INSERT INTO "toms_lookups"."UnacceptableTypes" ("Code", "Description") VALUES (11, 'Informal Crossover (Vehicles)');
-INSERT INTO "toms_lookups"."UnacceptableTypes" ("Code", "Description") VALUES (12, 'Cycle lane');
+INSERT INTO "toms_lookups"."UnacceptableTypes" ("Code", "Description") VALUES (12, 'Bus/Cycle lane');
 INSERT INTO "toms_lookups"."UnacceptableTypes" ("Code", "Description") VALUES (13, 'Distributor Road - Narrow');
