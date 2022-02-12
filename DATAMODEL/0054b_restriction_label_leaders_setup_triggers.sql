7dfa075a6fc11344830671fec82583a3a738745d
tlh22 <thancock@mhtc.co.uk>
2022-02-12 18:06:26 +0100

    split trigger from setup of triggers

---
+++ DATAMODEL/0054b_restriction_label_leaders_setup_triggers.sql
@@ -0,0 +1,64 @@
+/****
+ * Set up triggers on tables
+ ***/
+
+DROP TRIGGER IF EXISTS insert_mngmt ON toms."Bays";
+DROP TRIGGER IF EXISTS z_insert_mngmt ON toms."Bays";
+
+CREATE TRIGGER z_insert_mngmt
+    BEFORE INSERT OR UPDATE OF "TimePeriodID", "MaxStayID", "NoReturnID", "MatchDayTimePeriodID", "AdditionalConditionID", "PermitCode", "geom", "label_pos"
+    ON toms."Bays"
+    FOR EACH ROW
+    EXECUTE FUNCTION toms.labelling_for_restrictions();
+
+DROP TRIGGER IF EXISTS insert_mngmt ON toms."Lines";
+DROP TRIGGER IF EXISTS z_insert_mngmt ON toms."Lines";
+
+CREATE TRIGGER z_insert_mngmt
+    BEFORE INSERT OR UPDATE OF "NoWaitingTimeID", "NoLoadingTimeID", "MatchDayTimePeriodID", "AdditionalConditionID", "geom", "label_pos", "label_loading_pos"
+    ON toms."Lines"
+    FOR EACH ROW
+    EXECUTE FUNCTION toms.labelling_for_restrictions();
+
+/*"""*/
+-- Run the trigger once to "repair" leaders
+
+--- Bays
+ALTER TABLE toms."Bays" DISABLE TRIGGER "set_last_update_details_bays";
+ALTER TABLE toms."Bays" DISABLE TRIGGER "set_create_details_Bays";
+ALTER TABLE toms."Bays" DISABLE TRIGGER "set_bay_geom_type_trigger";
+ALTER TABLE toms."Bays" DISABLE TRIGGER "set_restriction_length_bays";
+ALTER TABLE toms."Bays" DISABLE TRIGGER "set_restriction_length_Bays";
+ALTER TABLE toms."Bays" DISABLE TRIGGER "update_capacity_bays";
+ALTER TABLE toms."Bays" DISABLE TRIGGER "notify_qgis_edit";
+
+UPDATE toms."Bays" SET label_pos = label_pos;
+
+ALTER TABLE toms."Bays" ENABLE TRIGGER "set_last_update_details_bays";
+ALTER TABLE toms."Bays" ENABLE TRIGGER "set_create_details_Bays";
+ALTER TABLE toms."Bays" ENABLE TRIGGER "set_bay_geom_type_trigger";
+ALTER TABLE toms."Bays" ENABLE TRIGGER "set_restriction_length_bays";
+ALTER TABLE toms."Bays" ENABLE TRIGGER "set_restriction_length_Bays";
+ALTER TABLE toms."Bays" ENABLE TRIGGER "update_capacity_bays";
+ALTER TABLE toms."Bays" ENABLE TRIGGER "notify_qgis_edit";
+
+--- Lines
+ALTER TABLE toms."Lines" DISABLE TRIGGER "set_last_update_details_lines";
+ALTER TABLE toms."Lines" DISABLE TRIGGER "set_create_details_Lines";
+ALTER TABLE toms."Lines" DISABLE TRIGGER "set_crossing_geom_type_trigger";
+ALTER TABLE toms."Lines" DISABLE TRIGGER "set_restriction_length_lines";
+ALTER TABLE toms."Lines" DISABLE TRIGGER "set_restriction_length_Lines";
+ALTER TABLE toms."Lines" DISABLE TRIGGER "update_capacity_lines";
+ALTER TABLE toms."Lines" DISABLE TRIGGER "notify_qgis_edit";
+
+UPDATE toms."Lines" SET label_pos = label_pos;
+
+ALTER TABLE toms."Lines" ENABLE TRIGGER "set_last_update_details_lines";
+ALTER TABLE toms."Lines" ENABLE TRIGGER "set_create_details_Lines";
+ALTER TABLE toms."Lines" ENABLE TRIGGER "set_crossing_geom_type_trigger";
+ALTER TABLE toms."Lines" ENABLE TRIGGER "set_restriction_length_lines";
+ALTER TABLE toms."Lines" ENABLE TRIGGER "set_restriction_length_Lines";
+ALTER TABLE toms."Lines" ENABLE TRIGGER "update_capacity_lines";
+ALTER TABLE toms."Lines" ENABLE TRIGGER "notify_qgis_edit";
+
+/*"""*/
--- DOCKER/postgis/toms_test_db_init_scripts/0054_restriction_label_leaders.sh
+++ DOCKER/postgis/toms_test_db_init_scripts/0054_restriction_label_leaders.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0054_restriction_label_leaders.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0054a_restriction_label_leaders_trigger.sql"
+psql -U postgres -d "TOMs_Test" -a -f "/io/DATAMODEL/0054b_restriction_label_leaders_setup_triggers.sql"
