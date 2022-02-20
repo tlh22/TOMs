13a62dcc21247fb9aba12929d409f66e58913fce
tlh22 <thancock@mhtc.co.uk>
2022-02-20 12:29:24 +0100

    Create 4007_fix_CPZ_1_5_boundary_issue.sql

---
+++ DATAMODEL/Sunday Parking (change CPZ times)/4007_fix_CPZ_1_5_boundary_issue.sql
@@ -0,0 +1,11 @@
+/***
+ *  As part of the changes for Sunday Parking, there is a small change to the geometry of the Zones 1 and 5 along Belford Road.
+ *  There is an issue with the boundary whereby the proposed changes for Zone 5 have been made to the current version. To remedy this, we need to:
+ *  a. Add in the correct version of Zone 5 (and make it current)
+ *  b. Within the relevant proposal, close the correct and open the amended version.
+ ***/
+
+-- Insert correct version
+
+
+--
