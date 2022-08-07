3091d646544119f5db791a88361656d472d6103c
tlh22 <thancock@mhtc.co.uk>
2022-08-08 07:21:24 +1000

    cec times

---
+++ test/data/0064_additional_time_periods.sql
@@ -0,0 +1,4 @@
+-- More ...
+
+INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (645, 'Mon-Sun 10.00am-1.00pm', 'Mon-Sun 10.00am-1.00pm');
+INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (646, 'Mon-Sun 7.00pm-midnight midnight-6.00am', 'Mon-Sun 7.00pm-midnight, midnight-6.00am');
