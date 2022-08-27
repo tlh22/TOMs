ba633daebc7ad6cc7e9182528bdb0ce4f296a3d6
tlh22 <thancock@mhtc.co.uk>
2022-08-27 11:30:28 +0200

    Create 0064_new_restriction_types.sql

---
+++ test/data/0064_new_restriction_types.sql
@@ -0,0 +1,3 @@
+-- Add section type
+INSERT INTO toms_lookups."BayLineTypes"("Code", "Description") VALUES (168, 'E-scooter and dockless bicycle bay');
+INSERT INTO toms_lookups."BayLineTypes"("Code", "Description") VALUES (169, 'Shared use bicycle and resident permit motorcycle');
