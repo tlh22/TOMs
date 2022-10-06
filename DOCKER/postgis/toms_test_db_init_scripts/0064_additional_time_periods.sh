79850cb63a8e19949151a7285a3c6e307f352111
tlh22 <thancock@mhtc.co.uk>
2022-10-06 09:35:08 +0200

    cambridge area

---
+++ DOCKER/postgis/toms_test_db_init_scripts/0064_additional_time_periods.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+psql -U postgres -d "TOMs_Test" -a -f "/io/test/data/0064_additional_time_periods.sql"
+
+
---
+++ test/data/0064_additional_time_periods.sql
@@ -0,0 +1,6 @@
+/**
+This now becomes the master dataset for time periods ...
+**/
+
+INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (645, 'Mon-Sat 8.15am-9.15am 5.00pm-6.00pm', 'Mon-Sat 8.15am-9.15am 5.00pm-6.00pm');
+INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (646, 'Mon-Fri 10.00am-6.00pm', 'Mon-Fri 10.00am-6.00pm');
