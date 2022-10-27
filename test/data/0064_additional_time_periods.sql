919d12104b87fae3227ba20d2a43a5230979be6a
tlh22 <thancock@mhtc.co.uk>
2022-10-27 13:13:06 +0200

    Update 0064_additional_time_periods.sql

--- test/data/0064_additional_time_periods.sql
+++ test/data/0064_additional_time_periods.sql
@@ -4,3 +4,5 @@ This now becomes the master dataset for time periods ...
 
 INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (645, 'Mon-Sat 8.15am-9.15am 5.00pm-6.00pm', 'Mon-Sat 8.15am-9.15am 5.00pm-6.00pm');
 INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (646, 'Mon-Fri 10.00am-6.00pm', 'Mon-Fri 10.00am-6.00pm');
+
+INSERT INTO "toms_lookups"."TimePeriods" ("Code", "Description", "LabelText") VALUES (647, 'Mon-Sat 11.00am-6.30pm', 'Mon-Sat 11.00am-6.30pm');
