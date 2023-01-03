65790a05fbf1c661f23b879be2943dccc256d0a6
tlh22 <thancock@mhtc.co.uk>
2023-01-03 15:36:56 +0100

    Update 1001_changes.sql

--- DATAMODEL/Specials/TRDC/Update_2022/1001_changes.sql
+++ DATAMODEL/Specials/TRDC/Update_2022/1001_changes.sql
@@ -11,4 +11,21 @@ WHERE "GeometryID" = 'L_0003956';
 DELETE FROM toms_lookups."TimePeriodsInUse"
 WHERE "Code" = 5;
 
--- TODO: Need to move crossovers to separate layer
+-- Move crossovers to separate layer
+
+SELECT setval('highway_assets."CrossingPoints_id_seq"', 1, true);
+
+INSERT INTO highway_assets."CrossingPoints"(
+	"RestrictionID", "RoadName", "USRN", "AssetConditionTypeID", "CreateDateTime", "CreatePerson", geom,
+	"CrossingPointTypeID")
+SELECT gen_random_uuid(), "RoadName", "USRN", 1, "CreateDateTime", "CreatePerson", geom,
+    CASE "UnacceptableTypeID"
+        WHEN 1 THEN 3
+        WHEN 4 THEN 1
+        WHEN 11 THEN 4
+	END CASE
+	FROM toms."Lines"
+	WHERE "UnacceptableTypeID"  IN (1, 4, 11)
+	AND  "RestrictionTypeID" IN (225);
+
+-- TODO: need to join unmarked lines so that they are continuous. Currently broken for crossovers. Also consider crossovers on SYLs
