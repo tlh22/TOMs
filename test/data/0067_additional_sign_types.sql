56aa5f50a4520dd35f24681c0378ea8658b0798a
tlh22 <thancock@mhtc.co.uk>
2025-07-27 10:32:44 +0200

    Update 0067_additional_sign_types.sql

--- test/data/0067_additional_sign_types.sql
+++ test/data/0067_additional_sign_types.sql
@@ -5,3 +5,5 @@ This now becomes the master dataset for sign types ...
 INSERT INTO "toms_lookups"."SignTypes" ("Code", "Description") VALUES (622, 'Parking - Disc Zone');
 
 INSERT INTO "toms_lookups"."AdditionalConditionTypes" ("Code", "Description") VALUES (32, 'Exempt from limited waiting');
+
+INSERT INTO "toms_lookups"."SignTypes" ("Code", "Description") VALUES (114, 'Parking - Cycle hire only');
